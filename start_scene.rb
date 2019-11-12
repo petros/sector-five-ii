39e8d96687dbe3b24489f453de075e267d5a6992
Petros Amiridis <petros@amiridis.net>
2019-11-12 06:59:22 +0200

    Use new start screen

--- start_scene.rb
+++ start_scene.rb
@@ -5,19 +5,19 @@ require_relative 'first_wave_scene'
 
 class StartScene < Scene
   def initialize
-    @background_image = Gosu::Image.new('images/start_screen.png')
+    @background_image = Gosu::Image.new('images/start_screen_ii.png')
     @start_music = Gosu::Song.new('sounds/lost_frontier.ogg')
     @start_music.play(true)
   end
-  
+
   def button_down(id)
     Game.current_scene = FirstWaveScene.new
     super
   end
-  
+
   def draw
     @background_image.draw(0,0,0)
     super
   end
-  
+
 end
